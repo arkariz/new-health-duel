b6cd6e53ba7f308cb71f14f862b5828c25ca3af8
arkariz <muhammadrisky1401@gmail.com>
2026-02-17 15:21:17 +0700

    refactor(error): remove unused error handling widgets and related exports

--- health_duel/lib/core/presentation/widgets/widgets.dart
+++ health_duel/lib/core/presentation/widgets/widgets.dart
@@ -5,7 +5,6 @@ export 'connectivity/connectivity.dart';
 export 'dialog/dialog.dart';
 export 'effect_listener.dart';
 export 'live_time_ago_text.dart';
-export 'error/error.dart';
 export 'form/form.dart';
 export 'responsive/responsive.dart';
 export 'shimmer/shimmer.dart';
